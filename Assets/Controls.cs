9e28ea5fe7a17a60f6093ee9f78bb6f74e71b9bf
Damien Corticchiato <skaradams69@gmail.com>
2018-12-30 20:48:44 +0100

    init Unity and setup test scene with prefabs

---
+++ Assets/Controls.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Controls : MonoBehaviour
+{
+  private Rigidbody2D body;
+  public float speed = 10.0F;
+
+  void Start() {
+    body = GetComponent<Rigidbody2D>();
+  }
+  void Update() {
+    float horizontalSpeed = Input.GetAxis("Horizontal") * speed;
+    float verticalSpeed = Input.GetAxis("Vertical") * speed;
+    Vector2 movement = new Vector2(horizontalSpeed, verticalSpeed);
+    body.MovePosition(body.position + movement * Time.fixedDeltaTime);
+  }
+}
